a32e664222b05e67d4e903cbf5f0030794f9bb65
John Taylor <JohnNoSpamTaylor@gmail.com>
2021-11-27 12:18:43 -0500

    add public_dns

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "instance_public_dns" {
+  description = "Public DNS address of the EC2 instance"
+  value       = aws_spot_instance_request.ec2_instance.public_dns
+}
+
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_spot_instance_request.ec2_instance.public_ip
--- resources.tf
+++ resources.tf
@@ -7,6 +7,6 @@ resource "aws_spot_instance_request" "ec2_instance" {
   spot_type                   = "one-time"
   instance_type               = var.inst_type
   provisioner "local-exec" {
-    command = "sleep 30; ssh-keyscan -t rsa ${aws_spot_instance_request.ec2_instance.public_ip} >> ~/.ssh/known_hosts"
+    command = "sleep 30; ssh-keyscan -t rsa ${aws_spot_instance_request.ec2_instance.public_dns} >> ~/.ssh/known_hosts; ssh-keyscan -t rsa ${aws_spot_instance_request.ec2_instance.public_ip} >> ~/.ssh/known_hosts"
   }
 }
